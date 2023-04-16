9cd61c489bae32ad82bdfc72605a20def52ebd64
Ruben Rajan <rubenrara@gmail.com>
2023-04-16 23:59:28 +0200

    disabeld python senor

--- code/Sensor.py
+++ code/Sensor.py
@@ -1,8 +1,8 @@
-#https://www.renesas.com/eu/en/document/dst/fs1015-datasheet?r=343956
-def readSensor(addres : int, testData : list) -> int:
-    print("reading from device: " , 0x0f , " at adress: ", addres, " return data: ", testData[0] , " <- low, high-> ", testData[1])
-    distance = (testData[0] << 8) | testData[1]
-    return distance
+# #https://www.renesas.com/eu/en/document/dst/fs1015-datasheet?r=343956
+# def readSensor(addres : int, testData : list) -> int:
+#     print("reading from device: " , 0x0f , " at adress: ", addres, " return data: ", testData[0] , " <- low, high-> ", testData[1])
+#     distance = (testData[0] << 8) | testData[1]
+#     return distance
 
 def createTesData(data : int) -> list:
     testData = divmod(data, 256)
